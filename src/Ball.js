e6ba5c65ae69e5b164a19a28ca2f9c96c865b550
Preet Dha <manpreetdha+github@gmail.com>
2018-04-08 20:38:28 -0700

    Using the Animated library, move ball from starting position, to another that is specified

--- src/Ball.js
+++ src/Ball.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Animated } from 'react-native';
 
 class Ball extends Component {
+  componentWillMount() {
+    this.position = new Animated.ValueXY(0, 0);
+    Animated.spring(this.position, {
+      toValue: { x: 200, y: 500 },
+    }).start();
+  }
   render() {
-    return <View style={styles.ball} />;
+    return (
+      <Animated.View style={this.position.getLayout()}>
+        <View style={styles.ball} />
+      </Animated.View>
+    );
   }
 }
 
@@ -13,8 +23,8 @@ const styles = {
     width: 60,
     borderRadius: 30,
     borderWidth: 30,
-    borderColor: 'black'
-  }
+    borderColor: 'black',
+  },
 };
 
 export default Ball;
